a5cc71f30662f55baac7c9f67206a52d36f53fc0
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2022-08-27 11:46:18 +0100

    Version changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='dummynn',    
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='A silly neural network algorithm for Python',
     author='Gleammer',
     license='MIT',
